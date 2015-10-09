5a2bc517216a2fc8afef4e1e5ad82088228bd7c9
kevin <kevinrosa@users.noreply.github.com>
2015-10-09 16:34:58 -0400

    json README
    made the the README files in JSON format.

--- tcm_2014_dirs.py
+++ tcm_2014_dirs.py
@@ -7,6 +7,7 @@ __author__ = 'kevinrosa'
 import os
 import glob  # for finding pathnames according to the rules used by the Unix shell
 import shutil  # I will use for moving files
+import json
 
 def main():
     database_dir = '/Users/kevinrosa/GSO/NBRIS_db/'
@@ -37,11 +38,11 @@ def main():
         for i in range(0, len([f for f in files_dat if serial in f])):
             shutil.copyfile(str([f for f in files_dat if serial in f][i]), new_dir+handle+'.dat')
 
-        make_README(new_dir, serial, handle)
+        make_README_json(new_dir, serial, handle)
 
 
 # The function to write the README:
-def make_README(new_dir, serial, handle):
+def make_README_json(new_dir, serial, handle):
     sn = list(map(str, [10175303, 10175307, 10175311, 10175312, 10175308, 10175306, 10175331, 9784975, 10175296, 2333586,
           2039045, 9784980, 9714529, 10175327, 10175324, 10175299, 10175323, 2006622, 2333584, 2006627,
           9714526, 9721595, 10175298, 9714531, 9714522, 9784979, 9714524, 9714532, 2333585, 9721594]))
@@ -66,27 +67,31 @@ def make_README(new_dir, serial, handle):
                          41.6687, 41.6303, 41.6669, 41.6617, 41.6532, 41.6639, 41.6563, 41.6556, 41.6551, 41.6403]))
     timezone = 'UTC'
     investigators = 'Chris Kincaid, Christelle Balt, Christina King Wertman, Sara Szwaja, Kevin Rosa, Tucker Sylvia'
-    project = 'SCOL 2014 (Study of Current on Ohio Ledge'
+    project = 'SCOL 2014 (Study of Current on Ohio Ledge)'
     velocity_units = 'cm/s'
 
     # Now let's write the README:
     if serial in sn:
         i = sn.index(serial)
-        file = open(new_dir+'README'+'_'+handle+'.txt', 'w')
-        file.write('SN: '+sn[i]+'\n')
-        file.write('Depth: '+depth[i]+'\n')
-        file.write('Y-axis: '+y_ax[i]+'\n')
-        file.write('Length: '+length[i]+'\n')
-        file.write('Lon: '+lon[i]+'\n')
-        file.write('Lat: '+lat[i]+'\n')
-        file.write('Timezone: '+timezone+'\n')
-        file.write('Velocity units: '+velocity_units+'\n')
-        file.write('Investigators: '+investigators+'\n')
-        file.write('Project name: '+project+'\n')
-        file.close()
+        data = {
+            'Sensor serial number' : sn[i],
+            'Depth' : depth[i],
+            'Y-axis' : y_ax[i],
+            'Length' : length[i],
+            'Lon' : lon[i],
+            'Lat' : lat[i],
+            'Time standard' : timezone,
+            'Velocity units' : velocity_units,
+            'Investigators' : investigators,
+            'Project name' : project
+        }
+        with open(new_dir+'README'+'_'+handle+'.txt', 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+            outfile.close()
 
     else:
-        file = open(new_dir+'README_ERROR.txt')
+        outfile = open(new_dir+'README_ERROR.txt', 'w')
+        outfile.close()
 
     return
 
